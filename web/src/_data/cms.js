e2726389ee0325ef55e5e2e758070e6cfaa7ab6f
bemayr <bernhard@mayr.io>
2022-06-21 13:12:19 +0200

    Added API v2 Experiments

--- web/src/_data/cms.js
+++ web/src/_data/cms.js
@@ -3,11 +3,11 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function() {
-  let url = "https://portal.lfrz.at/at.gv.lfrz.youngcarers-p/api/content/sync";
+  let url = "http://localhost:3000/api/v2/";
 
 	/* This returns a promise */
 	const response = await Cache(url, {
-		duration: "4h", // save for 4 hours
+		duration: "15min", // save for 4 hours
 		type: "json" // we’ll parse JSON for you
 	});
 
