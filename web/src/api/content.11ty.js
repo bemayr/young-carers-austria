b24e28713cb7a87c9bc9121815121554f1e879a1
bemayr <bernhard@mayr.io>
2022-06-22 15:01:23 +0200

    Fixed invalid API mapping

--- web/src/api/content.11ty.js
+++ web/src/api/content.11ty.js
@@ -2,5 +2,5 @@ module.exports = {
   data: {
     permalink: (data) => `api/${data.page.fileSlug}.json`,
   },
-  render: (data) => JSON.stringify(data.cms.entries, null, 2)
+  render: (data) => JSON.stringify(data.cms, null, 2)
 }
