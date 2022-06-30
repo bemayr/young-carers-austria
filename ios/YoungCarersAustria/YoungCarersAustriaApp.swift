2ad7f7b77b1f85b1c065f70ed2bac446df6ce915
bemayr <bernhard@mayr.io>
2022-06-30 07:41:44 +0200

    bootstrapped project

---
+++ ios/YoungCarersAustria/YoungCarersAustriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoungCarersAustriaApp.swift
+//  YoungCarersAustria
+//
+//  Created by Bernhard Mayr on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct YoungCarersAustriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
