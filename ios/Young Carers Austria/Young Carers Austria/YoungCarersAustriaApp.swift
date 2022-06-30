366aeaad06c8833d565ccca705312dc9c5a9b9d5
Bernhard Mayr <bemayr@users.noreply.github.com>
2022-06-30 13:26:47 +0200

    iOS Setup and Data Loading (#30)
    * setup git subfolder
    
    * added Gemfile + `bundle install`
    
    * bootstrapped project
    
    * recreated project
    
    * Added Dataloading

---
+++ ios/Young Carers Austria/Young Carers Austria/YoungCarersAustriaApp.swift
@@ -0,0 +1,18 @@
+//
+//  Young_Carers_AustriaApp.swift
+//  Young Carers Austria
+//
+//  Created by Bernhard Mayr on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct YoungCarersAustriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(ViewModel())
+        }
+    }
+}
