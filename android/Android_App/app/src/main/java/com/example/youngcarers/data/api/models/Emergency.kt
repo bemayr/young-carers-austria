fb124d2de110c3a8323a9973e0d56a922f6ed32f
cbachl <bachl.christoph@gmx.at>
2022-07-19 12:53:55 +0200

    edit

--- android/Android_App/app/src/main/java/com/example/youngcarers/data/api/models/Emergency.kt
+++
@@ -1,9 +0,0 @@
-package com.example.youngcarers.data.api.models
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Emergency(
-    @SerializedName("state")
-    val state: String
-)
