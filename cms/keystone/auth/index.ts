86e96462ea6d7ee6cc85b4ff77eb065787ad426e
bemayr <bernhard@mayr.io>
2022-09-19 15:23:28 +0200

    Improved Welcome Screen

--- cms/keystone/auth/index.ts
+++ cms/keystone/auth/index.ts
@@ -38,6 +38,7 @@ const { withAuth } = createAuth({
     // If there are no items in the database, keystone will ask you to create
     // a new user, filling in these fields.
     fields: ['name', 'email', 'password'],
+    skipKeystoneWelcome: true,
   },
 });
 
