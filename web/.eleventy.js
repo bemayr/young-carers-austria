6cce6986ada61d315a685ee0080fbbc1b28c7030
bemayr <bernhard@mayr.io>
2022-09-20 06:55:04 +0200

    Fixed Subdomain Issues
    - made www. the main domain prefix
    - removed eleventy pathPrefix

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -16,11 +16,6 @@ module.exports = function(eleventyConfig) {
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
-  var pathPrefix = "";
-  if (process.env.GITHUB_REPOSITORY) {
-    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
-  }
-
   // Markdown Filter
   const md = new markdownIt({
     html: true,
@@ -34,7 +29,6 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src"
     },
-    pathPrefix
   }
 };
 
