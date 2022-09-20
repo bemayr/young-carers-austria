fa9de4ff51b952ee030063a71572c10f0902f4c4
Bernhard Mayr <bemayr@users.noreply.github.com>
2022-09-20 18:52:42 +0200

    Added Alltagssituationen to CMS (#46)
    * Added Stories
    
    * Cleaned up v2 API

--- cms/keystone/_utils/markdown.ts
+++ cms/keystone/_utils/markdown.ts
@@ -1,36 +1,37 @@
 import { Node, Text } from "slate";
 import escapeHtml from "escape-html";
 
-export function slateToMarkdown(documentValue: any) {
-  function serialize(node: Node): string {
-    if (Text.isText(node)) {
-      let string = escapeHtml(node.text);
-      if (node.bold) {
-        string = `**${string}**`;
-      }
-      return string;
+export function nodeToMarkdown(node: Node): string {
+  if (Text.isText(node)) {
+    let string = escapeHtml(node.text);
+    if (node.bold) {
+      string = `**${string}**`;
     }
-
-    const children = node.children.map((n) => serialize(n)).join("");
-
-    switch (node.type) {
-      case "paragraph":
-        return `${children}\r\n\r\n`;
-      case "link":
-        return `[${children}](${escapeHtml(node.href)})`;
-      case "unordered-list":
-        return `\r\n${node.children
-          .map((n) => `* ${serialize(n)}`)
-          .join(`\r\n`)}\r\n\r\n`;
-      case "ordered-list":
-        return `\r\n${node.children
-          .map((n) => `1. ${serialize(n)}`)
-          .join(`\r\n`)}\r\n\r\n`;
-      default:
-        return children;
-    }
+    return string;
   }
+
+  const children = node.children.map((n) => nodeToMarkdown(n)).join("");
+
+  switch (node.type) {
+    case "paragraph":
+      return `${children}\r\n\r\n`;
+    case "link":
+      return `[${children}](${escapeHtml(node.href)})`;
+    case "unordered-list":
+      return `\r\n${node.children
+        .map((n) => `* ${nodeToMarkdown(n)}`)
+        .join(`\r\n`)}\r\n\r\n`;
+    case "ordered-list":
+      return `\r\n${node.children
+        .map((n) => `1. ${nodeToMarkdown(n)}`)
+        .join(`\r\n`)}\r\n\r\n`;
+    default:
+      return children;
+  }
+}
+
+export function slateToMarkdown(documentValue: any) {
   const node = { children: JSON.parse(documentValue) };
-  const markdown = serialize(node).trim();
+  const markdown = nodeToMarkdown(node).trim();
   return markdown;
 }
--- cms/keystone/server/api.v2.ts
+++ cms/keystone/server/api.v2.ts
@@ -5,6 +5,9 @@ import {
   KeystoneContext,
 } from "@keystone-6/core/types";
 import { gql } from "@keystone-6/core";
+import { Node, Text } from "slate";
+import escapeHtml from "escape-html";
+import { nodeToMarkdown } from "../_utils/markdown";
 
 const category = {
   transform: (category: any) => ({
@@ -40,189 +43,25 @@ export function registerAPIv2(
   app.get("/api/v2", async (req, res) => {
     const context: KeystoneContext = await createContext(req, res);
 
-    const insights = [
-      {
-        question: "Wie sieht ein typischer Tag eines Young Carers aus?",
-        content: [
-          {
-            type: "text",
-            text: "Das ist ein bisschen ein **Markdown**-Text, der sporadische Formatierungen zulässt.\n\nDie nächste Zeile stellt einen Verweis auf eine Referenz dar:",
-          },
-          {
-            type: "reference",
-            reference: {
-              url: "https://broschuerenservice.sozialministerium.at/Home/Download?publicationId=175",
-              title: "24-Stunden-Betreuung",
-              description:
-                'Broschüre "24-Stunden-Betreuung zu Hause" als Download',
-              previewImageUrl:
-                "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-              isPaidContent: false,
-              lastUpdated: "2022-04-12T08:43:35.740Z",
-              keywords: ["Sozialministerium"],
-            },
-          },
-          {
-            type: "text",
-            text: "Nun kommt wieder ein bisschen Erklärungstext, damit hier ein bisschen ein Text steht...",
-          },
-          {
-            type: "category",
-            category: {
-              name: "24-Stunden-Betreuung",
-              title: "24-Stunden-Betreuung",
-              information:
-                "Eine 24-Stunden-Betreuung ist den ganzen Tag sowie die ganze Nacht da, um die pflegebedürftige Person zu betreuen. Dabei spielt die Qualität eine ganz wichtige Rolle und wird auch mit einem Qualitätszertifikat für Vermittlungsagenturen ausgezeichnet.",
-              entries: [
-                {
-                  ownerName: "ÖQZ-24",
-                  ownerUrl: "",
-                  references: [
-                    {
-                      url: "https://oeqz.at/",
-                      title:
-                        "Gesicherte Qualität bei der 24-Stunden-Pflege (geändert)",
-                      description:
-                        "Für Agenturen der 24-Stunden-Betreuung gibt es ein freiwilliges Qualitätszertifikat.",
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-06-16T13:23:16.713Z",
-                      keywords: ["ÖQZ-24"],
-                    },
-                  ],
-                },
-                {
-                  ownerName: "Sozialministerium",
-                  ownerUrl: "",
-                  references: [
-                    {
-                      url: "https://www.sozialministerium.at/Themen/Pflege/24-Stunden-Betreuung.html",
-                      title: "24-Stunden-Betreuung",
-                      description:
-                        "Für betreuungsbedürftige Personen kann es notwendig sein, dass in privaten Haushalten Tag und Nacht eine Betreuungsperson anwesend sein muss. Dies erfolgt im Rahmen einer 24-Stunden-Betreuung. Um die wichtige Qualität sicherzustellen, wird für Vermittlungsagenturen ein Qualitätszertifikat verliehen.",
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-04-12T08:43:35.345Z",
-                      keywords: ["Sozialministerium"],
-                    },
-                    {
-                      url: "https://broschuerenservice.sozialministerium.at/Home/Download?publicationId=175",
-                      title: "24-Stunden-Betreuung",
-                      description:
-                        'Broschüre "24-Stunden-Betreuung zu Hause" als Download',
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-04-12T08:43:35.740Z",
-                      keywords: ["Sozialministerium"],
-                    },
-                  ],
-                },
-              ],
-            },
-          },
-        ],
-      },
-      {
-        question: "Wie könnte eine weitere Alltagssituation aussehen?",
-        content: [
-          {
-            type: "text",
-            text: "Eine **24-Stunden-Betreuung** ist den ganzen Tag sowie die ganze Nacht da, um die pflegebedürftige Person zu betreuen. Dabei spielt die Qualität eine ganz wichtige Rolle und wird auch mit einem [Qualitätszertifikat für Vermittlungsagenturen](https://oeqz.at) ausgezeichnet.",
-          },
-          {
-            type: "reference",
-            reference: {
-              url: "https://broschuerenservice.sozialministerium.at/Home/Download?publicationId=175",
-              title: "24-Stunden-Betreuung",
-              description:
-                'Broschüre "24-Stunden-Betreuung zu Hause" als Download',
-              previewImageUrl:
-                "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-              isPaidContent: false,
-              lastUpdated: "2022-04-12T08:43:35.740Z",
-              keywords: ["Sozialministerium"],
-            },
-          },
-          {
-            type: "reference",
-            reference: {
-              url: "https://www.gesundheit.gv.at/news/aktuelles.html",
-              title: "Gesundheitsportal",
-              description: "Aktuelle Themen des Gesundheitsportal Österreichs",
-              previewImageUrl:
-                "https://www.gesundheit.gv.at/dam/jcr:88ead326-91df-4fa9-b615-adf5ca365e39/logo-gesundheit%20(1).png",
-              isPaidContent: false,
-              lastUpdated: "2022-07-18T14:59:28.495Z",
-              keywords: ["Gesundheitsportal Österreichs"],
-            },
-          },
-          {
-            type: "text",
-            text: "Nun kommt wieder ein bisschen Erklärungstext, damit hier ein bisschen ein Text steht... Allerdings ist dieser Text um einiges länger und enthält auch **fetten**, *kursiven* und ~durchgestrichenen~ Text.\r\n\r\n> Außerdem ist das ein Zitat.",
-          },
-          {
-            type: "category",
-            category: {
-              name: "24-Stunden-Betreuung",
-              title: "24-Stunden-Betreuung",
-              information:
-                "Eine 24-Stunden-Betreuung ist den ganzen Tag sowie die ganze Nacht da, um die pflegebedürftige Person zu betreuen. Dabei spielt die Qualität eine ganz wichtige Rolle und wird auch mit einem Qualitätszertifikat für Vermittlungsagenturen ausgezeichnet.",
-              entries: [
-                {
-                  ownerName: "ÖQZ-24",
-                  ownerUrl: "",
-                  references: [
-                    {
-                      url: "https://oeqz.at/",
-                      title:
-                        "Gesicherte Qualität bei der 24-Stunden-Pflege (geändert)",
-                      description:
-                        "Für Agenturen der 24-Stunden-Betreuung gibt es ein freiwilliges Qualitätszertifikat.",
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-06-16T13:23:16.713Z",
-                      keywords: ["ÖQZ-24"],
-                    },
-                  ],
-                },
-                {
-                  ownerName: "Sozialministerium",
-                  ownerUrl: "",
-                  references: [
-                    {
-                      url: "https://www.sozialministerium.at/Themen/Pflege/24-Stunden-Betreuung.html",
-                      title: "24-Stunden-Betreuung",
-                      description:
-                        "Für betreuungsbedürftige Personen kann es notwendig sein, dass in privaten Haushalten Tag und Nacht eine Betreuungsperson anwesend sein muss. Dies erfolgt im Rahmen einer 24-Stunden-Betreuung. Um die wichtige Qualität sicherzustellen, wird für Vermittlungsagenturen ein Qualitätszertifikat verliehen.",
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-04-12T08:43:35.345Z",
-                      keywords: ["Sozialministerium"],
-                    },
-                    {
-                      url: "https://broschuerenservice.sozialministerium.at/Home/Download?publicationId=175",
-                      title: "24-Stunden-Betreuung",
-                      description:
-                        'Broschüre "24-Stunden-Betreuung zu Hause" als Download',
-                        previewImageUrl:
-                          "https://www.sozialministerium.at/dam/jcr:3c0e6acd-b52b-48c7-baae-b1b97065c162/Webbilder_YoungCarers-App_Bubble-closeup10.jpg",
-                      isPaidContent: false,
-                      lastUpdated: "2022-04-12T08:43:35.740Z",
-                      keywords: ["Sozialministerium"],
-                    },
-                  ],
-                },
-              ],
-            },
-          },
-        ],
-      },
-    ];
+    const insights = (
+      (
+        await context.graphql.run({
+          query: gql`
+            query {
+              stories {
+                name
+                content {
+                  document(hydrateRelationships: true)
+                }
+              }
+            }
+          `,
+        })
+      ).stories as any[]
+    ).map((story) => ({
+      question: story.name,
+      content: serializeStory(story.content.document),
+    }));
 
     const categories = (
       (
@@ -287,6 +126,85 @@ export function registerAPIv2(
 
     const timestamp = new Date().toISOString();
 
-    res.json({ insights, abc: categories, emergency, metadata, timestamp });
+    res.json({
+      insights,
+      abc: categories,
+      emergency,
+      metadata,
+      timestamp,
+    });
   });
+}
+
+type Story = Array<
+  | { type: "text"; text: string }
+  | { type: "reference"; reference: any }
+  | { type: "category"; category: any }
+>;
+export function serializeStory(documentValue: any): Story {
+  let result = [];
+
+  function serialize(node: Node, story: Story): Story {
+    function extendStoryWith(text: string): Story {
+      const last = story.at(-1);
+      if (last?.type === "text") {
+        last.text += text;
+        return story;
+      } else {
+        return [...story, { type: "text", text: text }];
+      }
+    }
+
+    if (Text.isText(node)) {
+      let string = escapeHtml(node.text);
+      if (node.bold) {
+        string = `**${string}**`;
+      }
+      return extendStoryWith(string);
+    }
+
+    if (node.type === "relationship") {
+      switch (node.relationship) {
+        case "reference":
+          return [...story, { type: "reference", reference: node.data.data }];
+        case "category":
+          return [...story, { type: "category", category: node.data.data }];
+      }
+    }
+
+    const children = node.children.map((n) => nodeToMarkdown(n)).join("");
+
+    switch (node.type) {
+      case "paragraph":
+        extendStoryWith(`${children}\r\n\r\n`);
+        break;
+      case "link":
+        extendStoryWith(`[${children}](${escapeHtml(node.href)})`);
+        break;
+      case "unordered-list":
+        return extendStoryWith(
+          `\r\n${node.children
+            .map((n) => `* ${nodeToMarkdown(n)}`)
+            .join(`\r\n`)}\r\n\r\n`
+        );
+      case "ordered-list":
+        return extendStoryWith(
+          `\r\n${node.children
+            .map((n) => `1. ${nodeToMarkdown(n)}`)
+            .join(`\r\n`)}\r\n\r\n`
+        );
+    }
+
+    return node.children
+      ? node.children.reduce(
+          (result, children) => serialize(children, result),
+          story
+        )
+      : story;
+  }
+  console.log(documentValue);
+  const markdown = documentValue
+    .flatMap((node) => serialize(node, []))
+    .filter((node) => !(node.type === "text" && node.text === ""));
+  return markdown;
 }
