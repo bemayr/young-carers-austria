9ffa2902b9cf5bf87e519396e6fdf18c8f09fbaf
bemayr <bernhard@mayr.io>
2023-05-30 15:03:48 +0200

    (web) add build hash to footer

--- web/src/_data/env.js
+++ web/src/_data/env.js
@@ -1,9 +1,31 @@
+const execSync = require('child_process').execSync;
+
 const environment = process.env.NODE_ENV;
-const isProduction = environment === "production";
-const baseUrl = environment === isProduction ? 'https://www.young-carers-austria.at' : 'http://localhost:8080';
+const isProduction = environment === 'production';
+const baseUrl =
+  environment === isProduction
+    ? 'https://www.young-carers-austria.at'
+    : 'http://localhost:8080';
+
+const now = new Date();
+const timeZone = 'UTC';
+const buildTime = new Intl.DateTimeFormat('de-AT', {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone,
+}).format(now);
+
+const latestGitCommitHash = execSync('git rev-parse --short HEAD')
+  .toString()
+  .trim();
 
 module.exports = {
   environment,
   isProduction,
-  baseUrl
+  baseUrl,
+  time: {
+    raw: now.toISOString(),
+    formatted: `${buildTime}`,
+  },
+  hash: latestGitCommitHash,
 };
